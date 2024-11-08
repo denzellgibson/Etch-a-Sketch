1f1d79ba4800237be96440fe2a8d77fb7f25e473
Denzell Gibson <19319491+denzellgibson@users.noreply.github.com>
2024-11-08 15:44:08 -0500

    Create function for square generation.

--- etchasketch.js
+++ etchasketch.js
@@ -9,17 +9,21 @@ GridContainer.style.width = "100vw";
 let GridHeight = 16;
 let GridWidth = 16;
 
-for (let i = 1; i <= GridHeight; i++) {
-    const GridRow = document.createElement("div");
-    GridRow.style.display = "flex";
-    GridRow.style.flex = "1";
-    GridRow.style.justifyContent = "space-evenly";
-    for (let j = 1; j <= GridWidth; j++) {
-        const GridSquare = document.createElement("div");
-        GridSquare.style.display = "flex";
-        GridSquare.style.flex = "1";
-        GridSquare.style.border = "black solid";
-        GridRow.appendChild(GridSquare);
+function SquareGridGeneration() {
+    for (let i = 1; i <= GridHeight; i++) {
+        const GridRow = document.createElement("div");
+        GridRow.style.display = "flex";
+        GridRow.style.flex = "1";
+        GridRow.style.justifyContent = "space-evenly";
+        for (let j = 1; j <= GridWidth; j++) {
+            const GridSquare = document.createElement("div");
+            GridSquare.style.display = "flex";
+            GridSquare.style.flex = "1";
+            GridSquare.style.border = "black solid";
+            GridRow.appendChild(GridSquare);
+        }
+        GridContainer.appendChild(GridRow);
     }
-    GridContainer.appendChild(GridRow);
 }
+
+SquareGridGeneration();
