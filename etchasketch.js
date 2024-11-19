3b7a872a30dfdea1e689dfa0d2631df8c227a5cb
Denzell Gibson <19319491+denzellgibson@users.noreply.github.com>
2024-11-19 05:44:55 -0500

    Decouple Square and Row creation from Grid Generation. Center the grid.

--- etchasketch.js
+++ etchasketch.js
@@ -1,26 +1,39 @@
+const BlockContainer = document.querySelector("#block-container");
+BlockContainer.style.display = "flex";
+BlockContainer.style.justifyContent = "center";
+// BlockContainer.style.border = 'green solid';
+
 const GridContainer = document.querySelector("#grid-container");
 GridContainer.style.display = "flex";
 GridContainer.style.flexDirection = "column";
 GridContainer.style.width = "960px";
 GridContainer.style.aspectRatio = "1/1";
+// GridContainer.style.border = 'red solid';
 
-let GridHeight = 16;
-let GridWidth = 16;
-
-function SquareGridGeneration() {
+function SquareGridGeneration(GridHeight, GridWidth) {
     for (let i = 1; i <= GridHeight; i++) {
-        const GridRow = document.createElement("div");
-        GridRow.style.display = "flex";
-        GridRow.style.flex = "1";
+        const GridRow = CreateGridRow();
         for (let j = 1; j <= GridWidth; j++) {
-            const GridSquare = document.createElement("div");
-            GridSquare.style.display = "flex";
-            GridSquare.style.flex = "1";
-            GridSquare.style.border = "black solid";
+            const GridSquare = CreateSquare();
             GridRow.appendChild(GridSquare);
         }
         GridContainer.appendChild(GridRow);
     }
 }
 
-SquareGridGeneration();
+function CreateSquare() {
+    const GridSquare = document.createElement("div");
+    GridSquare.style.display = "flex";
+    GridSquare.style.flex = "1";
+    GridSquare.style.border = "black solid";
+    return GridSquare;
+}
+
+function CreateGridRow() {
+    const GridRow = document.createElement("div");
+    GridRow.style.display = "flex";
+    GridRow.style.flex = "1";
+    return GridRow;
+}
+
+SquareGridGeneration(16, 16);
