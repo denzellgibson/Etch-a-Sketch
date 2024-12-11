b16ed24d04d48ac9dd81679f90d9cf7ffe07c0f8
Denzell Gibson <19319491+denzellgibson@users.noreply.github.com>
2024-12-11 02:26:22 -0500

    Add color fill on mouseover for squares on grid.

--- etchasketch.js
+++ etchasketch.js
@@ -8,7 +8,13 @@ GridContainer.style.display = "flex";
 GridContainer.style.flexDirection = "column";
 GridContainer.style.width = "960px";
 GridContainer.style.aspectRatio = "1/1";
-// GridContainer.style.border = 'red solid';
+GridContainer.style.border = 'red solid';
+
+GridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.id === 'square') {
+        e.target.style.background = 'red';
+    }
+})
 
 function SquareGridGeneration(GridHeight, GridWidth) {
     for (let i = 1; i <= GridHeight; i++) {
@@ -23,9 +29,11 @@ function SquareGridGeneration(GridHeight, GridWidth) {
 
 function CreateSquare() {
     const GridSquare = document.createElement("div");
+    GridSquare.addEventListener
     GridSquare.style.display = "flex";
     GridSquare.style.flex = "1";
     GridSquare.style.border = "black solid";
+    GridSquare.setAttribute('id', 'square');
     return GridSquare;
 }
 
